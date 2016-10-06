94e50d372d3a68f4e4d5b72ef150dd5d675a9406
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2016-10-06 14:54:57 +0300

    Adds comments to amity.py

--- buildings/amity.py
+++ buildings/amity.py
@@ -9,6 +9,8 @@ class Amity(object):
         pass 
 
     def create_room(name, type):
+        """creates a room depending on the type specified"""
+
         if type = "office":
             room = Office(name)
             room.save()
@@ -17,19 +19,25 @@ class Amity(object):
             room.save()
 
     def assign_room(person):
+        """randomly assigns a room to the person that it is passed"""
         return "" 
 
     def reallocate(person, room):
+        """assigns a person to the specified room if it is free"""
         pass
 
     def print_allocations():
+        """returns a printout of all rooms and persons assigned to them"""
         return "" 
 
     def save_state(database):
+        """saves current system data in a specified database"""
         pass 
 
     def load_state(database):
+        """loads data from a specified database into the system"""
         pass 
 
     def load_people(filename):
+        """loads people into the system from specified file"""
         pass 
