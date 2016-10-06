f84db547e07de3bdad708fc510e9471df9791427
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2016-10-06 15:24:52 +0300

    Feature - Person Class Breaks the name into first and last name

--- people/person.py
+++ people/person.py
@@ -5,8 +5,9 @@ class Person(object):
 
     allocated_office = ""
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name 
     
     def save(self):
         pass 
